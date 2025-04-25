280ec2a983802953d4772f3195ee0cc40f31c164
Jingjing Yang <jingjing.yang@tuni.fi>
2025-04-25 10:14:11 +0300

    Make filter mout plate cover the whole box face

--- opticalFilterMountSlot.py
+++ opticalFilterMountSlot.py
@@ -3,16 +3,16 @@ import FreeCAD, Part
 # -------------------------------------------------------
 # Create new FreeCAD document and set shaded wireframe
 # -------------------------------------------------------
-FreeCAD.newDocument('opticalFilterMountModule')
+FreeCAD.newDocument('opticalFilterMountModel')
 FreeCAD.Gui.runCommand('Std_DrawStyle', 6)
 
 # -------------------------------------------------------
 # 1. Filter Mount Block (17.8 x 1.9 x 29.8 mm)
 #     - Centered at (0, 0), starts at Z = 0
 # -------------------------------------------------------
-mount_width =  17.8       # along X
-mount_length = 1.9      # along Y
-mount_height = 29.8      # along Z
+mount_width =  18 - 0.2       # along X
+mount_length = 2 - 0.1      # along Y
+mount_height = 30 - 0.2      # along Z
 
 mount_origin = FreeCAD.Vector(
     -mount_width / 2,
@@ -29,10 +29,10 @@ mount = Part.makeBox(
 )
 
 # -------------------------------------------------------
-# 2. Base Plate (35 x 60.5 x 2 mm)
+# 2. Base Plate (50 x 60.5 x 2 mm)
 #     - Shifted so the base is offset at X = 7.5, Y = -13.75 from mount center
 # -------------------------------------------------------
-base_width = 35.0
+base_width = 50.0
 base_length = 60.5
 base_height = 2.0
 
@@ -40,7 +40,7 @@ plate_offset_x = 7.5
 plate_offset_y = -13.75
 
 plate_origin = FreeCAD.Vector(
-    -base_width / 2 + plate_offset_x,
+    -base_width / 2,
     -base_length / 2 + plate_offset_y,
     -base_height  # moved to be under the mount: Z = -2
 )
@@ -60,11 +60,11 @@ model = plate.fuse(mount)
 #     - Centered on mount
 #     - 17 mm up from base plate (Z=2)
 # -------------------------------------------------------
-hole_radius = 7.2
+hole_radius =5.2
 hole_length = mount_length + 0.2
 
 hole_center_x = 0
-hole_center_z = base_height + 17  # from bottom
+hole_center_z = base_height + 15  # from bottom
 
 hole_position = FreeCAD.Vector(
     hole_center_x,
@@ -80,7 +80,7 @@ model = model.cut(hole_cylinder)
 # -------------------------------------------------------
 # 4. Show Result in FreeCAD
 # -------------------------------------------------------
-shape = Part.show(model, 'opticalFilterMountModule')
+shape = Part.show(model, 'opticalFilterMountModel')
 shape.ViewObject.Transparency = 0
 shape.ViewObject.ShapeColor = (0.2, 0.6, 0.8)
 
